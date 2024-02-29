4c4bfc5e2504eba323afa733fc3cf630f2f4fcbb
Samaria <samafrajura@gmail.com>
2024-02-29 08:32:11 -0600

    08 React Server Components - use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hola mundo pagina de inicio ')
   return (
     <main className={styles.main}>
     <h1>Hola Mundo </h1>
