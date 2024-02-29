3b87f99ad734d02b35609e0d22bf1b798a33bf28
Samaria <samafrajura@gmail.com>
2024-02-28 20:18:09 -0600

    05 Como crear Layout en next

---
+++ src/app/store/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layaout({ chindren }:{chindren: React.ReactNode}){
+    return(
+
+        <main>
+<nav>Navegador de las categorias</nav>
+{chindren}
+
+        </main>
+    )
+}
