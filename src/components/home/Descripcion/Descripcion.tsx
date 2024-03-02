524920efacbb4bb85f65d4fc1de1f585a1620291
Samaria <samafrajura@gmail.com>
2024-03-02 13:37:33 -0600

    2_5 Manejo y optimizacion de imagenes

--- src/components/home/Descripcion/Descripcion.tsx
+++ src/components/home/Descripcion/Descripcion.tsx
@@ -1,9 +1,26 @@
+import { getPriority } from 'os'
+import styles from './Description.module.sass'
+import Image from 'next/image'
+
 export const Descripcion = () =>{
 return(
-    <section>
-        <img src="/images/description.jpeg" alt="Products marketplace"/>
-        <h2>Description</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga natus inventore animi enim autem esse et voluptatem asperiores! Quis magni suscipit quo atque ullam placeat architecto dolores quod quos ipsum!</p>
+    <section className={styles.Description}>
+        <Image
+         src="/images/description.jpeg" 
+         alt="Products marketplace"
+         width={500} 
+         height={300}
+         priority={false}
+         quality={100}
+         />
+
+       <div className={styles.Description__text}>
+        
+        <h2>Bring the future today</h2>
+<p>Future World: Your Geteway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear.Stay ahead of the curve and redefine ypur digital lifestyle with us.</p>
+   </div>
     </section>
 )
 }
+
+
