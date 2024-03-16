1b20889d960b243727f72501f05b4768c49f7b76
Samaria <samafrajura@gmail.com>
2024-03-16 09:58:06 -0600

    3_2 Manejo de variables de entorno

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,16 @@
+"use client"
+
 export const MianProducts = () =>{
+
+    console log( "veriable de entorno " process .env.NEXT_PUBLIC_HOPIFY_HOSTNAME)
+
     return(
         <section>
             <h1>MianProducts</h1>
         </section>
     )
     }
+
+function log(arg0: string, NEXT_PUBLIC_HOPIFY_HOSTNAME: string | undefined) {
+    throw new Error("Function not implemented.")
+}
