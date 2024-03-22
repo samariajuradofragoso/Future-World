10bccb883047fdaf3bcfc41b728c1a5bb7ed7343
Samaria <samafrajura@gmail.com>
2024-03-21 21:13:25 -0600

    4_11 Como funciona el fetch y el cache

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -3,7 +3,7 @@ import { env } from "app/config/env"
 export const shopifyUrls = {
   products: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-10/products.json`,
-    mainProducts: `${env.SHOPIFY_HOSTNAME}admin/api/2024-01/products/11235813213455.json`,
+    mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/collections/301884801223/products.json`,
   },
   collections: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/smart_collections.json`,
